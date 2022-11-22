e05bddcb688f32e5daaba578b708c138efaa3f48
Raven Ontiveros <98349647+RaveBlue@users.noreply.github.com>
2022-11-21 21:27:05 -0700

    Add files via upload

---
+++ ajaxHelpers.js
@@ -0,0 +1,31 @@
+// Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
+const cohortName = '2209-FTB-ET-WEB-PT';
+// Use the APIURL variable for fetch requests
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-PT/players/`;
+
+
+export const fetchAllPlayers = async () => {
+try{
+  const response = await fetch(`${APIURL}players`);
+    const result = await response.json();
+    if (result.error) {
+        throw result.error;
+    }
+    return result.data.players;
+  } catch (error) {
+    console.error('Uh oh, trouble fetching players!', error);
+  }
+
+};
+
+export const fetchSinglePlayer = async (playerId) => {
+
+};
+
+export const addNewPlayer = async (playerObj) => {
+
+};
+
+export const removePlayer = async (playerId) => {
+
+};
